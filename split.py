bf59be2c71c5736751dcf97ab29f6bf9bba1f105
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-04-07 22:36:37 +0700

    split added
    Split module added

---
+++ split.py
@@ -0,0 +1,9 @@
+def split(line,delim):
+    s=[]
+    j=0
+    for i in range (len(line)):
+        if delim== line [i]:
+            s.append(line[j:i])
+            j=i+1
+    s.append (line[j:])
+    return [ item for item in s if item ]
