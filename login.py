cfd648d9a9b3faf0ab98c5a14ed901789499cffe
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-04-08 23:41:31 +0700

    login added

--- login.py
+++ login.py
@@ -1,8 +1,17 @@
-from Modules import data,hash
+from Modules import data,hash,split
+
+# if error = 0, it means the username is not registered in the database 
+# if error = 1, it means that the password is incorrect
+
 
 def login():
-    users = data.loadData('Data/user.csv')
-    print(users)
-    return users
+    users = ''.join(str(p) for p in data.loadData('Data/user.csv'))
+    #print(users)
+    #print(users)
+    username = input("Masukan Username : ")
+    if username not in users : return '0'
+    password = hash.Hash(input("Masukan Password : "))
+    if password not in users : return '1'
+    return username
     
 
