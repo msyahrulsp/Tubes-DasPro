d127e63a9169672e3fb33a03f04c0c606dcefda9
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-04-08 23:30:44 +0700

    register fixed

--- register.py
+++ register.py
@@ -1,4 +1,4 @@
-from Modules import hash,data
+from Modules import hash,data,split
 
 #role
 
@@ -8,13 +8,23 @@ from Modules import hash,data
 #set unique id to id, but how????
 
 #idenya sih len(data) = id si user yg baru gabung
+# buat cek is username is unique, ntar bikin fungsi baru
+
+def isUsernameUnique(username,data): 
+    for i in data:
+        if split.split(i)[1] == username: return True
+    return False
+
 def register(): 
-    banyakdata = data.loadData('Data/user.csv')
+    mydata = data.loadData('Data/user.csv')
     fieldnames = ['id','username','nama','alamat','password','role']
     nama = input('Masukan nama : ')
     username = input('Masukan username : ')
+    if isUsernameUnique(username,mydata) is True: return '0' # if the return value, that means that the username is not unique, thus we will not continue the registration
     password = hash.Hash(input('Masukan password : '))
     alamat = input('Masukan alamat : ')
-    data.appendData('Data/user.csv',fieldnames,{'id':len(banyakdata),'username':username,'nama':nama,'alamat': alamat,'password':password,'role':0})
+    data.appendData('Data/user.csv',fieldnames,{'id':len(mydata),'username':username,'nama':nama,'alamat': alamat,'password':password,'role':0})
 
 register()
+
+
