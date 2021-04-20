8a1ba8ac1e8947519762f1f056b1cef39967be9e
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-04-21 00:11:41 +0700

    add and delete added
    Add and Delete File added

--- Modules/add.py
+++ Modules/add.py
@@ -13,13 +13,8 @@ def addGadget(data,id):
     if not (rare in validRarity):
         print("\nInput rarity tidak valid!")
     year = input("Masukan Tahun Ditemukan      : ")
-<<<<<<< HEAD
     print('Item telah berhasil ditambahkan ke database')
     return data+[[id,nama,desc,jlh,rare,year]]
-=======
-    print('\nItem telah berhasil ditambahkan ke database')
-    return data.append([id,nama,desc,jlh,rare,year])
->>>>>>> 077d323ececff01d4693b388b05e0830cb0f15f7
 
 
 def addConsumable(data,id):
@@ -30,13 +25,6 @@ def addConsumable(data,id):
     desc = input("Masukan Deskripsi            : ")
     jlh = input("Masukan Jumlah               : ")
     rare = input("Masukan Rarity               : ")
-<<<<<<< HEAD
     print('Item telah berhasil ditambahkan ke database')
     return data+[[id,nama,desc,jlh,rare]]
-=======
-    if not (rare in validRarity):
-        print("\nInput rarity tidak valid!")
-    print('\nItem telah berhasil ditambahkan ke database')
-    return data.append([id,nama,desc,jlh,rare])
->>>>>>> 077d323ececff01d4693b388b05e0830cb0f15f7
 
