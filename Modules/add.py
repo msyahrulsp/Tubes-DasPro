077d323ececff01d4693b388b05e0830cb0f15f7
M Syahrul Surya Putra <65522975+msyahrulsp@users.noreply.github.com>
2021-04-20 15:54:27 +0700

    add validasi rarity in add.py, integrate deleted

--- Modules/add.py
+++ Modules/add.py
@@ -1,26 +1,32 @@
 from Modules import search
 
+validRarity = ["C", "B", "A", "S"]
+
 def addGadget(data,id):
     if search.searchByID(data,id):
-        print(f"Gagal menambahkan karena {id} sudah ada")
+        print(f"\nGagal menambahkan karena {id} sudah ada")
         return data
     nama = input("Masukan Nama                 : ")   
     desc = input("Masukan Deskripsi            : ")
     jlh = input("Masukan Jumlah               : ")
     rare = input("Masukan Rarity               : ")
+    if not (rare in validRarity):
+        print("\nInput rarity tidak valid!")
     year = input("Masukan Tahun Ditemukan      : ")
-    print('Item telah berhasil ditambahkan ke database')
+    print('\nItem telah berhasil ditambahkan ke database')
     return data.append([id,nama,desc,jlh,rare,year])
 
 
 def addConsumable(data,id):
     if search.searchByID(data,id):
-        print(f"Gagal menambahkan karena {id} sudah ada")
+        print(f"\nGagal menambahkan karena {id} sudah ada")
         return data
     nama = input("Masukan Nama                 : ")   
     desc = input("Masukan Deskripsi            : ")
     jlh = input("Masukan Jumlah               : ")
     rare = input("Masukan Rarity               : ")
-    print('Item telah berhasil ditambahkan ke database')
+    if not (rare in validRarity):
+        print("\nInput rarity tidak valid!")
+    print('\nItem telah berhasil ditambahkan ke database')
     return data.append([id,nama,desc,jlh,rare])
 
