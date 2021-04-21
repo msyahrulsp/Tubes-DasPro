26bf08e39e87eec42bc40faf9ebdc213e8e33e35
Muhammad Rifqi Riansyah <73292530+riftkey@users.noreply.github.com>
2021-04-22 06:30:10 +0700

    Add files via upload
    F07 Mengubah Jumlah

---
+++ Modules/editstock.py
@@ -0,0 +1,24 @@
+#Module
+from Util.validasi import getId
+
+def EditStock(data,id):
+    idx = getId(data, id)
+    if idx == -1:
+        print("Tidak ada item dengan ID tersebut!")
+        return data
+    else:
+        jmlh = int(input("Masukkan Jumlah: "))
+        if (jmlh<0) and (data[idx][3]<abs(jmlh)):
+            print(f"{jmlh} {data[idx][1]} gagal dibuang karena stok kurang. Stok sekarang: {data[idx][3]} (< {jmlh})")
+            return data
+        elif jmlh>=0:
+            print(f"{jmlh} {data[idx][1]} berhasil ditambahkan. Stok sekarang: {data[idx][3]+jmlh}")
+            data[idx][3] = str(int(data[idx][3] + jmlh))
+        elif jmlh <0:
+            print(f"{jmlh} {data[idx][1]} berhasil dibuang. Stok sekarang: {data[idx][3] + jmlh}")
+            data[idx][3] = str(int(data[idx][3] + jmlh))
+    return data
+
+
+
+
