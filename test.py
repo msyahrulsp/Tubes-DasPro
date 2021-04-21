38ae4b3ca73414e257d6e7d482ea527eaa68073d
M Syahrul Surya Putra <65522975+msyahrulsp@users.noreply.github.com>
2021-04-22 01:10:31 +0700

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-for root, dirs, files in os.walk("./Data/2021-04-01_235959", topdown=False):
-   for name in files:
-      print((os.path.join(root, name)))
-   print("------------------")
-   for name in dirs:
-      print(os.path.join(root, name))
