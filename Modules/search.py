5bc207bb4d0f9277b813f012db1bb352e702c797
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-05-03 05:34:28 +0700

    kategori fixed

--- Modules/search.py
+++ Modules/search.py
@@ -25,15 +25,18 @@ searchByRarity(consumable,'S')
 def searchByYear(data,request,type): #data = datanya yg mau di search, request = tahun yang diperlukan, type = tipenya
     state = True
     print("\nHasil pencarian: ")
-    for newdata in data[1:]:
-        if eval(f'{int(newdata[5])} {type} {request}'):
-            state = False
-            print(f'\nNama            : {newdata[1]} ')
-            print(f'Deskripsi       : {newdata[2]} ')
-            print(f'Jumlah          : {newdata[3]} ')
-            print(f'Rarity          : {newdata[4]} ')
-            print(f'Tahun Ditemukan : {newdata[5]} ')  
-    if state :
+    try :
+        for newdata in data[1:]:
+            if eval(f'{int(newdata[5])} {type} {request}'):
+                state = False
+                print(f'\nNama            : {newdata[1]} ')
+                print(f'Deskripsi       : {newdata[2]} ')
+                print(f'Jumlah          : {newdata[3]} ')
+                print(f'Rarity          : {newdata[4]} ')
+                print(f'Tahun Ditemukan : {newdata[5]} ')
+        if state :
+            print('Tidak ada gadget ditemukan\n')
+    except:
         print('Tidak ada gadget ditemukan\n')
 
 # ex penggunaan : 
